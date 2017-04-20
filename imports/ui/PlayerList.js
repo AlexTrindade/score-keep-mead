151d9dcf83472fcd4d4ceefd1b8e17b93afb3cb6
Alex Trindade <trindadealex@gmail.com>
2017-04-19 23:59:32 -0300

    initial commit

---
+++ imports/ui/PlayerList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import FlipMove from 'react-flip-move';
+
+import Player from './Player';
+
+export default class PlayerList extends React.Component {
+
+  renderPlayers() {
+    if (this.props.players.length === 0) {
+      return (
+        <div className="item">
+          <p className="item__message">Add your first player to get started!</p>
+        </div>
+      );
+    } else {
+      return this.props.players.map((player) => {
+        return (
+          <Player key={player._id} player={player}/>
+        );
+      });
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="player-list">
+        <FlipMove maintainContainerHeight={true}>
+          {this.renderPlayers()}
+        </FlipMove>
+      </div>
+    )
+  }
+}
+
+PlayerList.propTypes = {
+  players: React.PropTypes.array.isRequired
+}
---
+++ server/main.js
@@ -0,0 +1,68 @@
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+
+Meteor.startup(() => {
+
+
+});
+
+
+// let square = function(x) {
+//   return x * x;
+// }
+
+// let square = (x) => x * x;
+// console.log(square(10));
+
+// let user = {
+//   name: 'Alex',
+//   sayHi: function() {
+//     setTimeout(() => {
+//       console.log(this.name);
+//     }, 1000);
+//     // console.log(arguments);
+//   }
+// }
+// user.sayHi(1,2);
+
+// let numbers = [9,99,4, 56];
+// let newNumbers = numbers.map((number) => number + 1);
+// console.log(numbers);
+// console.log(newNumbers);
+
+
+// SPREAD OPERATOR
+// let user = {
+//   name: 'Alex',
+//   location: 'Petrolina',
+//   age: 0
+// }
+// let person = {
+//   age: 25,
+//   ...user
+// }
+// console.log(person);
+
+// OBJECT PROPERTY SHORTHAND
+
+// let bike = 'scott';
+// let stuff = {
+//   bike
+// }
+
+// console.log(stuff);
+
+// let house = {
+//   bedrooms: 2,
+//   bathrooms: 1.5
+// }
+//
+// let yearBuilt = 1995;
+//
+//
+// console.log({
+//   ...house,
+//   bedrooms: 3,
+//   yearBuilt,
+//   flooring: 'Carpet'
+// });
